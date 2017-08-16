f4f259e3b56a1ab38949a5fc2180206fb8874f1b
phpradio <allphpeverything@gmail.com>
2017-08-16 09:56:09 -0700

    setting up API

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const routes = require('./api/routes');
-const config = require('./config/config');
-const cors = require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express     = require('express');
+const app         = express();
+const path        = require('path');
+const routes      = require('./api/routes');
+const config      = require('./config/config');
+const cors        = require('cors');
+const morgan      = require('morgan');
+const bodyParser  = require('body-parser');
 
 // app.set('port',3000);
 if (app.get('env') !== 'production') app.use(cors());
 app.use(morgan('dev'));
 
 app.use(function(req,res,next){
- console.log(req.method,req.url);
- next();
+  console.log(req.method,req.url);
+  next();
 });
 
 app.use(express.static(path.join(__dirname,'public')));
